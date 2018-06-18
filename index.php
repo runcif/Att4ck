f349967920244009c299bb3cacfe156a17c99f2b
Runci Francesco <32999037+runcif@users.noreply.github.com>
2018-06-18 22:07:41 +0200

    inzialize

--- index.php
+++ index.php
@@ -13,7 +13,9 @@
 	    <td style="text-align: center;">Drone Recognize</td>
     </tr>
   <tr>
-    <td style="text-align: center;"><button name="cerca">Start</button></td>
+	<td style="text-align: center;"><button name="mode">Avvia il Monitor Mode</button></td>
+    <td style="text-align: center;"><button name="start">Avvia Ricerca Drone</button></td>
+    <td style="text-align: center;"><button name="deuth">Distruggi il Drone</button></td>
   </tr>
  </tbody>
   </table>
@@ -23,17 +25,40 @@
 
 <?php 
 
-if (isset($_POST['cerca']))
+if (isset($_POST['mode']))
 {
 		
-	exec("cd /usr/lib/cgi-bin && sudo iwlist wlan1 scan | iw_parse", $output, $code);
+	exec("cd /usr/lib/cgi-bin && sudo airmon-ng | grep -Eo 'wlan[0-9]'", $output, $code);
 	switch($code) {
     case 0:
-    echo $output;
+    
+    
+    if($output>0) :
+    
+    ?>
+
+<table style="width: 75%; text-align: center; margin-left: auto; margin-right: auto;"
+ border="0" cellpadding="2" cellspacing="2">
+	    
+	    <tr>
+        <th>Nome scheda</th>
+    </tr>
+    
+    <?php foreach ($output as $row): ?>
+    
+
+<tr>
+	        <td> <a href="<?php echo "select_wlan.php?wlan=".$row;?>"><?php echo $row;?></a><td>
+
+
+    </tr>
+    <?php endforeach;endif; ?>
+</table>
+    
+    <?php 
     
      break;
    }
-}
 }
 ?>
 	
