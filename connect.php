a63683389dcf85273b00a30664d7ce997d183a34
Runci Francesco <32999037+runcif@users.noreply.github.com>
2018-06-23 00:11:25 +0200

    rimane solamente il db dei droni

---
+++ connect.php
@@ -0,0 +1,53 @@
+<?php
+$servername = "localhost";
+$username = "runcif";
+$password = "att4ck";
+$dbname = "att4ck";
+
+try
+{
+$conn =  new PDO("mysql:host=127.0.0.1;dbname=$dbname", $username, $password);
+
+}
+catch(PDOException $e)
+{
+
+      echo $e->getMessage();
+    
+    
+}
+
+
+function get_mac($conn){
+
+$sql    = "SELECT * FROM drone";
+ $result = $conn->prepare($sql);
+        $result->execute();
+   return $result; 
+
+
+}
+
+function set_drone($output,$conn){
+
+$query = "INSERT INTO drone (drone_name, mac_add) VALUES
+            ('$output[0]', '$output[1]')";
+
+$result = $conn->prepare($query);
+$result->execute();
+
+}
+
+function check_drone($mac,$conn){
+
+$sql    = "SELECT * FROM drone WHERE mac_add = ".$mac;
+ $result = $conn->prepare($sql);
+        $result->execute();
+   return $result; 
+
+}
+
+
+
+
+?>
