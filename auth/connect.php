82a22f6788f72850cbef0d83265d94c27420e770
Runci Francesco <32999037+runcif@users.noreply.github.com>
2018-07-19 21:12:06 +0200

    Update connect.php

---
+++ auth/connect.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "localhost";
+$username = "runcif";
+$password = "att4ck";
+$dbname = "att4ck";
+
+try
+{
+$conn =  new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+}
+catch(PDOException $e)
+{
+
+      echo $e->getMessage();
+
+
+}
+
+
+
+function check_vendor($mac,$conn){
+
+$sql    = "SELECT vendor FROM ouiList WHERE oui LIKE '%".$mac."%'";
+ $result = $conn->prepare($sql);
+        $result->execute();
+   return $result->fetch();
+
+}
+
+function check_drone($mac,$conn){
+$sql    = "SELECT drone FROM ouiList WHERE oui LIKE '%".$mac."%'";
+ $result = $conn->prepare($sql);
+        $result->execute();
+   return $result->fetch();
+
+}
+
+function set_drone($mac,$conn){
+
+$sql    = "UPDATE ouiList SET drone = 1 WHERE oui LIKE '%".$mac."%'";
+ $result = $conn->prepare($sql);
+        $result->execute();
+
+}
+
+
+
+?>
